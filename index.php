018580c4fe9804e55a5ebafc42a7e393609308f0
lexx54 <chapelexx54@gmail.com>
2021-02-03 18:57:08 -0400

    changes the names of the css files

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Cambridge Language Institute</title>
   <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="css/main.css">
-  <link rel="stylesheet" href="css/main720.css">
-  <link rel="stylesheet" href="css/main1020.css">
+<link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/style720.css">
+  <link rel="stylesheet" href="css/style1020.css">
+
 </head>
 <body>
   <nav></nav>
