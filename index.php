da6615b28564ac919abc11d4baa0003c84f12a95
lexx54 <chapelexx54@gmail.com>
2021-02-02 21:26:55 -0400

    added css relatives links in the index and reset file

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cambridge Language Institute</title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/main720.css">
+  <link rel="stylesheet" href="css/main1020.css">
+</head>
+<body>
+  <nav></nav>
+
+  <header></header>
+
+  <main>
+
+  </main>
+
+  <footer></footer>
+  <script src="scripts.js" type="module"></script>
+</body>
+</html>
